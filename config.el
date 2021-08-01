e0ddedb65dbaf379b72d733971ebc68393430991
Ji Zhang <jzhang68@buffalo.edu>
2021-08-01 09:49:33 -0700

    os specific settings

--- config.el
+++ config.el
@@ -55,4 +55,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq mac-command-modifier 'meta)
+(cond
+ ((string-equal system-type "darwin") ; macOS
+  (setq mac-command-modifier 'meta)))
